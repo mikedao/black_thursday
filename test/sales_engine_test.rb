71f301ae20bbd4b0d9d5ece582cd304d052c06e4
Michael Dao <michael.dao@gmail.com>
2017-05-31 09:58:12 -0600

    Added first SE Test

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,17 @@
+require "./test/test_helper"
+require "./lib/sales_engine"
+
+class SalesEngineTest < Minitest::Test
+
+  def test_se_takes_csv_hashes
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+
+    assert_instance_of se, SalesEngine
+  end
+
+
+
+end
