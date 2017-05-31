58913bdd61c64d2a807551dbd1369cbb14466c1e
Michael Dao <michael.dao@gmail.com>
2017-05-31 13:30:26 -0600

    Adds unit price to dollars

--- lib/item.rb
+++ lib/item.rb
@@ -4,6 +4,7 @@ class Item
   attr_reader   :name,
             :description,
             :unit_price,
+            :merchant_id,
             :created_at,
             :updated_at
 
@@ -11,7 +12,12 @@ class Item
     @name         = data[:name]
     @description  = data[:description]
     @unit_price   = BigDecimal.new(data[:unit_price].to_i)
+    @merchant_id  = data[:merchant_id].to_i
     @created_at   = Time.new(data[:created_at])
     @updated_at   = Time.new(data[:updated_at])
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f / 100
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -7,13 +7,15 @@ class ItemTest < Minitest::Test
               :name        => "Pencil",
               :description => "You can use it to write things",
               :unit_price  => "3800",
+              :merchant_id => "1",
               :created_at  => "2016-01-11 20:59:20 UTC",
-              :updated_at  => "2009-12-09 12:08:04 UTC",
+              :updated_at  => "2009-12-09 12:08:04 UTC"
     })
 
     assert_equal "Pencil", i.name
     assert_equal "You can use it to write things", i.description
     assert_equal BigDecimal.new(3800), i.unit_price
+    assert_equal 1, i.merchant_id
     assert_equal Time.new("2016-01-11 20:59:20 UTC"), i.created_at
     assert_equal Time.new("2009-12-09 12:08:04 UTC"), i.updated_at
   end
@@ -23,14 +25,43 @@ class ItemTest < Minitest::Test
               :name        => "Bob Gu",
               :description => "Best Gu",
               :unit_price  => "3800000",
+              :merchant_id => "2",
               :created_at  => "2017-01-11 20:59:20 UTC",
-              :updated_at  => "2002-12-09 12:08:04 UTC",
+              :updated_at  => "2002-12-09 12:08:04 UTC"
     })
 
     assert_equal "Bob Gu", i.name
     assert_equal "Best Gu", i.description
     assert_equal BigDecimal.new(3800000), i.unit_price
+    assert_equal 2, i.merchant_id
     assert_equal Time.new("2017-01-11 20:59:20 UTC"), i.created_at
     assert_equal Time.new("2002-12-09 12:08:04 UTC"), i.updated_at
   end
+
+  def test_unit_price_to_dollars
+    i = Item.new({
+              :name        => "Pencil",
+              :description => "You can use it to write things",
+              :unit_price  => "3875",
+              :merchant_id => "1",
+              :created_at  => "2016-01-11 20:59:20 UTC",
+              :updated_at  => "2009-12-09 12:08:04 UTC"
+    })
+
+    assert_equal 38.75, i.unit_price_to_dollars
+  end
+
+  def test_different_unit_price_to_dollars
+    i = Item.new({
+              :name        => "Pencil",
+              :description => "You can use it to write things",
+              :unit_price  => "4075",
+              :merchant_id => "1",
+              :created_at  => "2016-01-11 20:59:20 UTC",
+              :updated_at  => "2009-12-09 12:08:04 UTC"
+    })
+
+    assert_equal 40.75, i.unit_price_to_dollars
+  end
+
 end
